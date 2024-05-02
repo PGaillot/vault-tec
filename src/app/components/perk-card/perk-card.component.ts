dc9693af7c5789239b1b2ace89cf57c349848ebb
PGaillot <pr.gaillot@gmail.com>
2024-05-03 01:32:04 +0200

    return card.

--- src/app/components/perk-card/perk-card.component.ts
+++ src/app/components/perk-card/perk-card.component.ts
@@ -1,13 +1,38 @@
 import { Component, Input } from '@angular/core'
 import { PerksType } from '../../enums/perks-type'
 import { NgClass } from '@angular/common'
+import {
+  trigger,
+  style,
+  state,
+  transition,
+  animate,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-perk-card',
   standalone: true,
-  imports: [NgClass],
+  imports: [NgClass ],
   templateUrl: './perk-card.component.html',
   styleUrl: './perk-card.component.scss',
+  animations:[
+        // version simple
+        trigger('openClose', [
+          state(
+            'open',
+            style({
+              transform:'rotateY(180deg)'
+            })
+          ),
+          state(
+            'closed',
+            style({
+              transform:'rotateY(0deg)',
+            })
+          ),
+          transition('* => *', [animate('0.5s')]),
+        ]),
+  ]
 })
 export class PerkCardComponent {
   @Input() content!: string
