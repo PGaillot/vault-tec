9ed234c3d7d59061f486fb22d332fddad03bb538
PGaillot <pr.gaillot@gmail.com>
2024-05-04 13:39:43 +0200

    add perks data.

---
+++ src/app/data/perks.data.ts
@@ -0,0 +1,46 @@
+import { Perk } from '../models/perk.model'
+
+export const PERKS: Perk[] = [
+  {
+    char: 'S',
+    name: 'Strong',
+    desc:
+      'Magni dolorum deserunt quod, iusto magnam quasi suscipit temporibus commodi, facilis soluta numquam hic impedit alias ratione unde rem! Asperiores ab, dolor recusandae enim eos?',
+  },
+  {
+    char: 'P',
+    name: 'Perception',
+    desc:
+      'Magni dolorum deserunt quod, iusto magnam quasi soluta numquam hic impedit alias ratione unde rem! Asperiores ab, dolor recusandae enim eos',
+  },
+  {
+    char: 'E',
+    name: 'Endurance',
+    desc:
+      'Magni dolorum deserunt quod, iusto magnam quasi suscipit temporibus commodi, facilis soluta numquam hic impedit alias ratione unde rem! As edita quam voluptatibus dolores recusandae enim eos?',
+  },
+  {
+    char: 'C',
+    name: 'Charisme',
+    desc:
+      'Magni dolorum deserunt quod, iusto magnam quasi suscipit temporibus commodi, facilis soluta numquam hic impedit alias ratione unde rem! Asperiores ab, dolorum, ta quam voluptatibus dolores recusandae enim eos?',
+  },
+  {
+    char: 'I',
+    name: 'Intelligence',
+    desc:
+      'Magni dolorum deserunt quod, iusto magnam quasi suscipit temporibus commodi, facilis soluta numquam hic impedit alias ratione unde rem! Asperiores ab, dolor recusandae enim eos?',
+  },
+  {
+    char: 'A',
+    name: 'Agilité',
+    desc:
+      'Magni dolorum deserunt quod, iusto magnam quasi suscipit temporibus commodi, facilis soluta numquam hic impedit alias ratione unde r recusandae enim eos?',
+  },
+  {
+    char: 'L',
+    name: 'Luck',
+    desc:
+      'Magni dolorum deserunt quod, iusto magnam quasi suscipit temporibus commodi, facilis soluta numquam hic impedit alias ratione unde rem! Aspe, dolor recusandae enim eos?',
+  },
+]
