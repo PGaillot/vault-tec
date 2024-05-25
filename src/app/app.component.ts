67bb4863622b89b761633bdad6b69b7775125f2a
PGaillot <pr.gaillot@gmail.com>
2024-05-25 12:46:48 +0200

    add sounds service.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,66 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { FooterComponent } from './components/footer/footer.component';
 import { NavMenuComponent } from './components/nav-menu/nav-menu.component';
+import { SoundsService } from './services/sounds.service';
+import { BehaviorSubject } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
+import {
+  FormControl,
+  FormControlName,
+  ReactiveFormsModule,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, FooterComponent, NavMenuComponent],
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    FooterComponent,
+    NavMenuComponent,
+    AsyncPipe,
+    ReactiveFormsModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'VAULT-TEC';
+  muted: BehaviorSubject<boolean>;
+  volume: FormControl = new FormControl(0.2);
+
+  constructor(private soundsService: SoundsService) {
+    this.muted = soundsService.muted;
+  }
+
+  toogleMute() {
+    this.soundsService.toggleMute();
+  }
+
+
+  onVolumeChange(volume: number) {
+    console.log('Volume changed to:', volume);
+    // Vous pouvez ajouter ici le code pour mettre à jour le volume dans le service
+    this.soundsService.volume.next(volume);
+  }
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+
+    this.volume.valueChanges.subscribe({
+      next: (value) => {
+
+        if(value){
+          this.onVolumeChange(value / 10)
+        }
+      },
+    });
+
+    // this.volume..subscribe({
+    //   next: (value) => {
+    //     console.log(value + '!!!');
+    //   },
+    // })
+  }
 }
--- src/app/services/sounds.service.ts
+++ src/app/services/sounds.service.ts
@@ -1,29 +1,56 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Subscription } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SoundsService {
 
-  constructor() { }
+  constructor() {
+    this.subscription = [
+      ...this.subscription, 
+    ]
+  }
 
-  volume:number = 0.1;
+  public volume:BehaviorSubject<number> = new BehaviorSubject<number>(0.2);
+  public muted:BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  subscription:Subscription[] = [];
+
+  mute(){
+    this.muted.next(true);
+  }
+
+  unmute(){
+    this.muted.next(false);
+  }
+
+  toggleMute(){
+    this.muted.next(!this.muted.value);    
+  }
 
   playAudioClick(){
+    if(this.muted.getValue()) return
     let audio = new Audio();
     audio.src = "../../../assets/sounds/pen-click.mp3";
     audio.load();
-    audio.volume = this.volume;
+    audio.volume = this.volume.getValue();
     audio.play();    
   }
-
+  
   playAudioError(){
+    if(this.muted.getValue()) return
     let audio = new Audio();
     audio.src = "../../../assets/sounds/console-error.mp3";
     audio.load();
-    audio.volume = this.volume;
+    audio.volume = this.volume.getValue();
     audio.play();    
     console.log('audio error')
+  }
+
+  ngOnDestroy(): void {
+    //Called once, before the instance is destroyed.
+    //Add 'implements OnDestroy' to the class.
+    this.subscription.forEach((sub:Subscription) => sub.unsubscribe())
   }
   
 }
